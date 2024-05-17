cb5bf6c9589d71a350c1de2838c02f6af1020ade
NoodleSushi <ytmikesstuffs@gmail.com>
2024-05-17 11:18:18 +0800

    fjk;lsadf

--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ setup(
   download_url = 'https://github.com/ajrequina/Cebuano-POS-Tagger/archive/1.0.tar.gz',
   keywords = ['pos-tagger', 'cebuano-pos-tagger'],
   classifiers = [],
+  include_package_data=True,
   install_requires=[
     'cebstemmer @ git+https://github.com/NoodleSushi/Cebuano-Stemmer.git',
     'nltk'
-  ]
+  ],
 )
